cfe41be0839265f5c8e9809c2780107f56d1dacc
EMon-BHai PROJECT <138991689+EMon-BHai-Github@users.noreply.github.com>
2025-09-14 21:24:56 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  ownerUsernames: ["@emonhowlader"],//Your Username
+  ownerUsernames: ["EMONHAWLADAR"],//Your Username
   ownerNumber: "+8801309991724",//Your Number
   admin: ["5079311859", "5079311859"], // your id
   prefix: "/", // dont change prefix 
@@ -7,7 +7,7 @@ module.exports = {
   imageUrl: "https://i.postimg.cc/5ycrKgKw/78fa584d9b11d33eb8155cbbcb98c96e.jpg",//Thumbnail Url
   po0rt: process.env.PORT || 8053 || 8053, // port
   mess: {
-    ownerlink: "https://t.me/@emonhowlader",
+    ownerlink: "https://t.me/EMONHAWLADAR",
     eror: "Internal Server Eror 😵",
     owner: "Sorry, this command can only be accessed by the owner!",
     group: "Sorry, this command can only be used within a group!",
